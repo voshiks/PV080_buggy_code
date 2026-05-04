214c964544d87c4225b62124bdcaaa5977b48e3d
Vladimir Kurganov <voshiks@gmail.com>
2026-05-04 16:54:28 +0200

    Fix #183

--- buggy_python_code_solution.py
+++ buggy_python_code_solution.py
@@ -53,7 +53,7 @@ if __name__ == '__main__':
     print("3. Yaml deserialization vulnerability: see file_solution.yaml for a solution")
     print("4. Use of assert statements vulnerability: run program with -O argument")
     choice  = input("Select vulnerability: ")
-    if choice == "1": 
+    if choice == "1":
         new_person = Person("Vickie")  
         print_nametag(input("Please format your nametag: "), new_person)
     elif choice == "2":
